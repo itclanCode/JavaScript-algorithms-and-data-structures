3c84be5fdca16e5047d33a50fdcd2ade57afd280
itclancode <itclancode@163.com>
2020-02-17 19:30:35 +0800

    if条件语句

---
+++ 数据结构与算法JavaScript描述/第1章-javascript编程坏境和模型/1.2.3-判断结构/简单的if语句.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 条件判断
+ * @Version: 1.0
+ * @Autor: vxPublic:itclanCoder
+ * @Date: 2020-02-16 14:47:57
+ * @LastEditors: 川川
+ * @LastEditTime: 2020-02-17 09:41:35
+ * 
+ * 条件语句是一种代码结构,用来测试表达式的真假,并根据布尔表达式的结果执行不同的代码,也就是
+ *  判断结构让程序可以选择执行哪些程序语句
+ */
+
+var sex = "男";
+if(sex === "男") {
+   console.log("你是个汉子");   
+}
+
