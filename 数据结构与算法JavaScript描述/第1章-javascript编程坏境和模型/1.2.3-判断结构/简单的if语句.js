faea342b2b257761b33169b8c1716df774375d37
itclancode <itclancode@163.com>
2020-02-24 19:41:24 +0800

    增加代码注释

--- 数据结构与算法JavaScript描述/第1章-javascript编程坏境和模型/1.2.3-判断结构/简单的if语句.js
+++ 数据结构与算法JavaScript描述/第1章-javascript编程坏境和模型/1.2.3-判断结构/简单的if语句.js
@@ -1,12 +1,15 @@
 /*
- * @Description: 条件判断
- * @Version: 1.0
+ * @LineStart: -------------------------------------------
+ * @Copyright: © 2020, itclanCoder. All rights reserved.
+ * @LineEnd: ----------------------------------------------
+ * @Product: 
+ * @Mode Name: 
  * @Autor: vxPublic:itclanCoder
  * @Date: 2020-02-16 14:47:57
+ * @Version: xxx.v1.0
  * @LastEditors: 川川
- * @LastEditTime: 2020-02-17 09:41:35
- * 
- * 条件语句是一种代码结构,用来测试表达式的真假,并根据布尔表达式的结果执行不同的代码,也就是
+ * @LastEditTime: 2020-02-24 19:40:33
+ * @Description: 条件语句是一种代码结构,用来测试表达式的真假,并根据布尔表达式的结果执行不同的代码,也就是
  *  判断结构让程序可以选择执行哪些程序语句
  */
 
